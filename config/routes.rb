65aba519a59de2fe8ab9d98a492de294dc8cd5f4
Ajay <ajaykanyal11@gmail.com>
2022-04-07 13:59:41 +0530

    Added a route in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      get 'ticket/init_settings' => 'ticket#init_settings'
       get 'ticket/index/:per_page/:page_no' => 'ticket#index'
       post 'ticket/read' => 'ticket#read'
       get 'ticket/create' => 'ticket#new'
