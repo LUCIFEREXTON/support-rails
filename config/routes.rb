00e98dc3508ad8c9d9105978ddad796ab5e85cd7
Ajay <ajaykanyal11@gmail.com>
2022-04-12 14:18:06 +0530

    Added ticket per request parameter

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get 'ticket/init_settings' => 'ticket#init_settings'
-      get 'ticket/index' => 'ticket#index'
-      post 'ticket/read' => 'ticket#read'
-      get 'ticket/create' => 'ticket#new'
-      post 'ticket/create' => 'ticket#create'
-      put 'ticket/update/:id' => 'ticket#update'
-      post 'ticket/reply/:id' => 'ticket#reply'
-			get 'ticket/blog_uri_list' => 'ticket#blog_uri_list'
+      namespace :freshdesk do
+	get 'ticket/init_settings' => 'ticket#init_settings'
+	get 'ticket/index' => 'ticket#index'
+	post 'ticket/read' => 'ticket#read'
+	get 'ticket/create' => 'ticket#new'
+	post 'ticket/create' => 'ticket#create'
+	put 'ticket/update/:id' => 'ticket#update'
+	post 'ticket/reply/:id' => 'ticket#reply'
+	get 'ticket/blog_uri_list' => 'ticket#blog_uri_list'
+      end
     end
   end
 
