e378d31534b158906ce5bbc898809719d65d40c8
Ajay <ajaykanyal11@gmail.com>
2022-04-13 17:23:56 +0530

    Added Custom Errors

--- lib/BlogVault/error.rb
+++ lib/BlogVault/error.rb
@@ -1,7 +1,48 @@
 module BlogVault
-	class Error < StandardError
-		def initialise(msg)
-			super(msg)
-		end
+	class NotFoundError < StandardError
+	  def initialize(params)
+		@params = params
+	  end
+  
+	  def message
+		"No #{@params} found"
+	  end
+	end
+  
+	class MissingParamsError < StandardError
+	  def initialize(params)
+		@params = params
+	  end
+  
+	  def message
+		"You have not sent the following required fields: #{@params.join(',')}"
+	  end
+	end
+  
+	class ServerError < StandardError
+	  def message
+		"Server Issue, Please Try Again..."
+	  end
+	end
+  
+	class NullValueError < StandardError
+	  def initialize(params) 
+		@params = params
+	  end
+  
+	  def message
+		"#{@params} passed is null."
+	  end
+	end
+  
+	class InvalidRequestError < StandardError
+	  def initialize(params) 
+		@params = params
+	  end
+  
+	  def message
+		"Invlid #{@params} format"
+	  end
 	end
 end
+  
